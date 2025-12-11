90007c6d122f3b93e733422a9f82f88ee2d86ff5
Paing Thu Tun <155553026+Paing-Thu-Tun@users.noreply.github.com>
2025-12-12 02:47:11 +1100

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string
+            .split("") // becomes array of characters e.g. "Hello" -> ['H', 'e', 'l', 'l', 'o'];
+            .reverse()
+            .join("");
 };
 
 // Do not edit below this line
