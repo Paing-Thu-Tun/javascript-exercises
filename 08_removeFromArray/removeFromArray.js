37bb8bef1ab2e2a892754cb8e2e59b9900b04c78
Paing Thu Tun <155553026+Paing-Thu-Tun@users.noreply.github.com>
2025-12-14 18:08:42 +1100

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(arr, ...items) {
+//     for (const item of items) {
+//         while (arr.includes(item)) {
+//             const index = arr.indexOf(item);
+//             arr.splice(index, 1);
+//         }
+//     }
+//     return arr;
+// };
+
+const removeFromArray = function(arr, ...args) {
+    const newArray = [];
+    arr.forEach(item => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
+
+// const removeFromArray = function(arr, ...args) {
+//     return arr.filter(val => !args.includes(val));
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
