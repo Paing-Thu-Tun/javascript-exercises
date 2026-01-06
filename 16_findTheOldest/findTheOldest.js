421a3396c4a3102ed997314218657065c2795479
Paing Thu Tun <155553026+Paing-Thu-Tun@users.noreply.github.com>
2026-01-07 00:06:20 +1100

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+        yearOfDeath = (new Date).getFullYear();
+    }
 
+    return yearOfDeath - yearOfBirth;
+}
+
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
